8469486dfa401f67a38377ea6533990e0dce76d7
Josep Lluís Marin Trabalón <101903899+josepLlMt20@users.noreply.github.com>
2024-05-06 22:18:48 +0200

    Crud methods afegits, falten vistes i comprovacio que funcioni tot

--- VPT-ProvaTecnica/src/Controller/ProveedorController.php
+++ VPT-ProvaTecnica/src/Controller/ProveedorController.php
@@ -16,17 +16,26 @@ class ProveedorController extends AbstractController
     /**
      * @Route("/proveedor", name="proveedor_index", methods={"GET"})
      */
-    public function index(): Response
+    public function getAll(): Response
     {
         $proveedores = $this->getDoctrine()->getRepository(Proveedor::class)->findAll();
 
         return $this->render('proveedor/index.html.twig', ['proveedores' => $proveedores]);
     }
+    /**
+     * @Route("/proveedor/{id}", name="proveedor_detalles", methods={"GET"})
+     */
+    public function getProveedor($id): Response
+    {
+        $proveedor = $this->getDoctrine()->getRepository(Proveedor::class)->find($id);
+
+        return $this->render('proveedor/detalles.html.twig', ['proveedor' => $proveedor]);
+    }
 
     /**
-     * @Route("/proveedor/nuevo", name="proveedor_nuevo", methods={"GET","POST"})
+     * @Route("/proveedor/create", name="proveedor_create", methods={"GET","POST"})
      */
-    public function nuevo(Request $request): Response
+    public function createProveedor(Request $request): Response
     {
         $proveedor = new Proveedor();
         $form = $this->createForm(Proveedor::class, $proveedor);
@@ -46,6 +55,36 @@ class ProveedorController extends AbstractController
         ]);
     }
 
-    // Otros métodos para editar, eliminar y ver detalles de proveedores
+    /**
+     * @Route("/proveedor/{id}/update", name="proveedor_update", methods={"GET","POST"})
+     */
+    public function updateProveedor(Request $request, $id): Response{
+        $proveedor = $this->getDoctrine()->getRepository(Proveedor::class)->find($id);
+        $form = $this->createForm(Proveedor::class, $proveedor);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('proveedor_index');
+        }
+
+        return $this->render('proveedor/editar.html.twig', [
+            'proveedor' => $proveedor,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/proveedor/{id}/delete", name="proveedor_delete", methods={"DELETE"})
+     */
+    public function deleteProveedor(Request $request, $id): Response{
+        $proveedor = $this->getDoctrine()->getRepository(Proveedor::class)->find($id);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($proveedor);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('proveedor_index');
+    }
 }
 
